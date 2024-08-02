01931f6ae48eb8070a6bce0b3b325236f460d9fe
omaromarsalhi <omar.salhi@esprit.tn>
2024-08-02 15:01:39 +0100

    integrating with the calcul ue and the calcul module

---
+++ services/calcul-Module/src/main/java/com/stage/calcul_Module/CalculModuleApplication.java
@@ -0,0 +1,13 @@
+package com.stage.calcul_Module;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculModuleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculModuleApplication.class, args);
+	}
+
+}
---
+++ services/calcul-Module/src/test/java/com/stage/calcul_Module/CalculModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stage.calcul_Module;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculModuleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
